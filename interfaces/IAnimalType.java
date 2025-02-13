442a204062d15b57f3c160ff03aa6816e879d087
maksim0840 <117035413+maksim0840@users.noreply.github.com>
2025-02-13 10:22:57 +0300

    Delete

--- interfaces/IAnimalType.java
+++
@@ -1,5 +0,0 @@
-package interfaces;
-
-public interface IAnimalType {
-    public boolean isInteractiveAllowed();
-}
--- interfaces/IApprover.java
+++
@@ -1,5 +0,0 @@
-package interfaces;
-
-public interface IApprover {
-    public boolean isApproved(int param);
-}
